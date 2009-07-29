b95cd78ac38ebc16e65294b09626703252c48c96
fotinakisii <fotinakisii@23ed9a0e-3f40-11de-b5fc-91815ed80701>
2009-07-29 21:55:57 +0000

    * Added: ability to have bindings from multiple dataforms present on one page * Removed: jQuery wrapping so that outer functions can call setBindings for Ajax loaded forms
    git-svn-id: http://django-dataforms.googlecode.com/svn/trunk@56 23ed9a0e-3f40-11de-b5fc-91815ed80701

--- js/bindings.js
+++ js/bindings.js
@@ -1,90 +1,91 @@
-;(function($) {
-	function setBinding(parent, child, choice, effectTime) {
-		parent.change(function() {
-			doBinding(parent, child, choice, effectTime);
-		});
-	}
-	
-	function doBinding(parent, child, choice, effectTime){
-		var boundArea = child.parent()
-		var effectTime = typeof(effectTime) != 'undefined' ? effectTime : 500;
-		var choice = typeof(choice) != 'undefined' ? choice : -1;
-		
-		if (!boundArea.length && window.console && window.console.log) {
-			console.log("Could do binding because boundArea was empty.")
-			console.log("Parent:")
-			console.log(parent)
-			console.log("Child:")
-			console.log(child)
-			return;
-		} else if (!boundArea.length) {
-			return;
-		}
-		
-		// This fixes subelements like multiple checkboxes
-		if (boundArea.attr("tagName").toLowerCase() == "label") {
-			switch (boundArea.parent().attr("tagName").toLowerCase()){
-			case "li":
-				boundArea = boundArea.parent().parent()
-				boundArea = boundArea.add(boundArea.prev())
-				break;
-			}
-		}
-		
-		// We can't just do slideToggle because this slide is going to be called
-		// once for every subelement bound to the parent, where slideDown and
-		// slideUp mask this behavior because they can be called multiple times
-		if (parent.attr("checked")
-			|| (parent.parent().find("input:checked").length && parent.val() == choice)
-			|| (parent.parent().find("select").length && parent.val() == choice)) {
-			boundArea.slideDown(effectTime);
-		} else {
-			boundArea.slideUp(effectTime);
-		}
-	}
-	
-	$(function() {
-	
-		// Create the binding event handlers
-		// FIXME: .val() is going to pull the first one's value, so this code doesn't
-		// work for multiple forms present on one page that all need bindings.
-		var bindings_js = $("input[type='hidden'][name*='js_dataform_bindings']").val()
+function setBinding(parent, child, choice, effectTime) {
+	parent.change(function() {
+		doBinding(parent, child, choice, effectTime);
+	});
+}
 
+function doBinding(parent, child, choice, effectTime){
+	var boundArea = child.parent()
+	var effectTime = typeof(effectTime) != 'undefined' ? effectTime : 500;
+	var choice = typeof(choice) != 'undefined' ? choice : -1;
+	
+	if (!boundArea.length && window.console && window.console.log && window.console.error) {
+		console.error("Could do binding because boundArea was empty.")
+		console.log("Parent:")
+		console.log(parent)
+		console.log("Child:")
+		console.log(child)
+		return;
+	} else if (!boundArea.length) {
+		return;
+	}
+	
+	// This fixes subelements like multiple checkboxes
+	if (boundArea.attr("tagName").toLowerCase() == "label") {
+		switch (boundArea.parent().attr("tagName").toLowerCase()){
+		case "li":
+			boundArea = boundArea.parent().parent().parent()
+			break;
+		}
+	}
+	
+	// We can't just do slideToggle because this slide is going to be called
+	// once for every subelement bound to the parent, where slideDown and
+	// slideUp mask this behavior because they can be called multiple times
+	if (parent.attr("checked")
+		|| (parent.parent().find("input:checked").length && parent.val() == choice)
+		|| (parent.parent().find("select").length && parent.val() == choice)) {
+		boundArea.slideDown(effectTime);
+		boundArea.show();
+	} else {
+		boundArea.slideUp(effectTime);
+		boundArea.hide();
+	}
+}
+
+function setBindings() {
+	// Create the binding event handlers
+	var many_bindings_js = $("input[type='hidden'][name*='js_dataform_bindings']");
+	var bindings_js = '';
+	
+	for (var j=0; j < many_bindings_js.length; j++) {
+		bindings_js = many_bindings_js[j].value;
+		
 		if (!bindings_js)
-			return;
-
+			continue;
+		
 		var bindings = JSON.parse(bindings_js);
-	
+		
 		for (var i = 0; i < bindings.length; i++) {
 			var parent = $("#id_" + bindings[i][0]);
 			var choiceVal = undefined;
 			var success = true;
-		
+			
 			if (bindings[i].length == 2) {
 				// Simple binding (like a single checkbox).
 				// (parent, child)
-			
+				
 				var child = $("#id_" + bindings[i][1]);
 				setBinding(parent, child);
-			
+				
 			} else if (bindings[i].length == 3) {
 				// Choice binding (like a dropdown)
 				// (parent, choice value, child)
-			
+				
 				var choiceVal = bindings[i][1];
 				var child = $("#id_" + bindings[i][2]);
 				if (!child.length){
 					child = $("input[name='"+ bindings[i][2]+"']").slice(0,1);
 				}
-			
+				
 				var parent = $("#id_" + bindings[i][0]);
 				if (!parent.length){
 					parent = $("input[name='"+ bindings[i][0]+"'][value='"+choiceVal+"']");
 				}
-			
+				
 				if (parent.length) {
 					var elementName = parent[0].tagName.toLowerCase();
-				
+					
 					if (elementName == "select") {
 						// Select element
 						setBinding(parent, child, choiceVal);
@@ -103,12 +104,14 @@
 							console.log(child);
 						}
 						success = false;
-						alert("Choice bindings for " + elementName + " elements has not been implemented.")
+						if (window.console && window.console.error){
+							console.error("Choice bindings for " + elementName + " elements has not been implemented.")
+						}
 					}
 				} else {
 					success = false;
-					if (window.console && window.console.log){
-						console.log("Couldn't find parent " + "#id_" + bindings[i][0])
+					if (window.console && window.console.error){
+						console.error("Couldn't find parent " + "#id_" + bindings[i][0])
 					}
 				}
 			}
@@ -118,5 +121,9 @@
 				doBinding(parent, child, choiceVal, 0);
 			}
 		}
-	});
-})(jQuery);
+	}
+}
+
+$(function() {
+	setBindings();
+});
