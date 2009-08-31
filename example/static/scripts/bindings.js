080cb6c14d4bf1d23fc7c7f1eaae099b5e4d72fd
mike.fotinakis <mike.fotinakis@23ed9a0e-3f40-11de-b5fc-91815ed80701>
2009-08-31 20:05:28 +0000

    * Added dummy data for writing of JavaScript
    git-svn-id: http://django-dataforms.googlecode.com/svn/branches/bindings-rewrite@86 23ed9a0e-3f40-11de-b5fc-91815ed80701

--- example/static/scripts/bindings.js
+++ example/static/scripts/bindings.js
@@ -20,15 +20,6 @@ function doBinding(parent, child, choice, effectTime){
 		return;
 	}
 	
-	// This fixes subelements like multiple checkboxes
-	if (boundArea.attr("tagName").toLowerCase() == "label") {
-		switch (boundArea.parent().attr("tagName").toLowerCase()){
-		case "li":
-			boundArea = boundArea.parent().parent().parent()
-			break;
-		}
-	}
-	
 	// We can't just do slideToggle because this slide is going to be called
 	// once for every subelement bound to the parent, where slideDown and
 	// slideUp mask this behavior because they can be called multiple times
@@ -54,77 +45,29 @@ function setBindings() {
 		if (!bindings_js)
 			continue;
 		
-		var bindings = JSON.parse(bindings_js);
+		//var bindings = JSON.parse(bindings_js);
+		var bindings = [
+		                // Single checkbox
+		                {
+		                	"parents" : [["personal-information__checkbox"]],
+		                	"children" : ["personal-information__dropdown"]
+		                },
+		                // Single dropdown with choice of "Yes"
+		                {
+		                	"parents" : [[["personal-information__dropdown", "yes"]]],
+		                	"children" : ["personal-information__date"]
+		                },
+		                // Dropdown with choice of "Yes" OR Checkbox checked
+		                {
+		                	"parents" : [[["personal-information__dropdown", "yes"]], ["personal-information__checkbox"]],
+		                	"children" : ["personal-information__checkbox"]
+		                }
+		                ];
+		                
 		
-		for (var i = 0; i < bindings.length; i++) {
-			var parent = $("#id_" + bindings[i][0]);
-			var choiceVal = undefined;
-			var success = true;
-			
-			if (bindings[i].length == 2) {
-				// Simple binding (like a single checkbox).
-				// (parent, child)
-				
-				var child = $("#id_" + bindings[i][1]);
-				if (!child.length){
-					child = $("input[name='"+ bindings[i][1]+"']").slice(0,1);
-				}
-				
-				setBinding(parent, child);
-				
-			} else if (bindings[i].length == 3) {
-				// Choice binding (like a dropdown)
-				// (parent, choice value, child)
-				
-				var choiceVal = bindings[i][1];
-				var child = $("#id_" + bindings[i][2]);
-				if (!child.length){
-					child = $("input[name='"+ bindings[i][2]+"']").slice(0,1);
-				}
-				
-				var parent = $("#id_" + bindings[i][0]);
-				if (!parent.length){
-					parent = $("input[name='"+ bindings[i][0]+"'][value='"+choiceVal+"']");
-				}
-				
-				if (parent.length) {
-					var elementName = parent[0].tagName.toLowerCase();
-					
-					if (elementName == "select") {
-						// Select element
-						if (window.console && window.console.log){
-							console.log("Adding select binding for " + "#id_" + bindings[i][0] + " --> " + "#id_" + bindings[i][2])
-						}
-						setBinding(parent, child, choiceVal);
-					} else if (elementName == "input" && parent[0].type == "radio") {
-						// Radio buttons
-						if (window.console && window.console.log){
-							console.log("Adding radio binding for " + "#id_" + bindings[i][0] + " --> " + "#id_" + bindings[i][2])
-						}
-						setBinding(parent, child, choiceVal);
-					} else {
-						if (window.console && window.console.log){
-							console.log(parent);
-							console.log(child);
-						}
-						success = false;
-						if (window.console && window.console.error){
-							console.error("Choice bindings for " + elementName + " elements has not been implemented.")
-						}
-					}
-				} else {
-					success = false;
-					if (window.console && window.console.error){
-						console.error("Couldn't find parent " + "#id_" + bindings[i][0])
-					}
-				}
-			}
-			
-			// Hide/show all bound fields default state
-			if (success) {
-				doBinding(parent, child, choiceVal, 0);
-			}
-		}
+		// Ready, go.
+		
+		
 	}
 }
 
