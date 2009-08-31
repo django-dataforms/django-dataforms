f878ccb78ab330dc1aadf60f12d2ace4528ff624
mike.fotinakis <mike.fotinakis@23ed9a0e-3f40-11de-b5fc-91815ed80701>
2009-08-31 20:06:55 +0000

    * Remove old cruft
    git-svn-id: http://django-dataforms.googlecode.com/svn/branches/bindings-rewrite@87 23ed9a0e-3f40-11de-b5fc-91815ed80701

--- example/static/scripts/bindings.js
+++ example/static/scripts/bindings.js
@@ -1,39 +1,3 @@
-function setBinding(parent, child, choice, effectTime) {
-	parent.change(function() {
-		doBinding(parent, child, choice, effectTime);
-	});
-}
-
-function doBinding(parent, child, choice, effectTime){
-	var boundArea = child.parent()
-	var effectTime = typeof(effectTime) != 'undefined' ? effectTime : 500;
-	var choice = typeof(choice) != 'undefined' ? choice : -1;
-	
-	if (!boundArea.length && window.console && window.console.log && window.console.error) {
-		console.error("Couldn't do binding because boundArea was empty.")
-		console.log("Parent:")
-		console.log(parent)
-		console.log("Child:")
-		console.log(child)
-		return;
-	} else if (!boundArea.length) {
-		return;
-	}
-	
-	// We can't just do slideToggle because this slide is going to be called
-	// once for every subelement bound to the parent, where slideDown and
-	// slideUp mask this behavior because they can be called multiple times
-	if (parent.attr("checked")
-		|| (parent.parent().find("input:checked").length && parent.val() == choice)
-		|| (parent.parent().find("select").length && parent.val() == choice)) {
-		boundArea.slideDown(effectTime);
-		boundArea.show();
-	} else {
-		boundArea.slideUp(effectTime);
-		boundArea.hide();
-	}
-}
-
 function setBindings() {
 	// Create the binding event handlers
 	var many_bindings_js = $("input[type='hidden'][name*='js_dataform_bindings']");
