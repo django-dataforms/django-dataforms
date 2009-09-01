e1331f32ed4fd309314be53d825545cef9ddec10
stmcallister <stmcallister@23ed9a0e-3f40-11de-b5fc-91815ed80701>
2009-09-01 14:54:11 +0000

    * putting meat on the bones of the bindings.js.
    git-svn-id: http://django-dataforms.googlecode.com/svn/branches/bindings-rewrite@88 23ed9a0e-3f40-11de-b5fc-91815ed80701

--- example/static/scripts/bindings.js
+++ example/static/scripts/bindings.js
@@ -30,9 +30,51 @@ function setBindings() {
 		                
 		
 		// Ready, go.
-		
-		
+		for (var k=0; k < bindings.length; k++) {
+			$(parent).data("parents", bindings[k]['parents']);
+			$(parent).data("children", bindings[k]['children']);
+			$(parent).change(doBinding());
+		}
 	}
+}
+
+function doBinding() {
+	parents = $(this).data('parents');
+	
+	truth = isTruth(parents);
+	
+	if(truth){
+		// show
+		parents.slideDown();
+		parents.show();
+	} else {
+		// hide
+		parents.slideUp();
+		parents.hide();
+	}
+}
+
+function isTruth(listOfParents) {
+	
+	for(var i=0; i < listOfParents; i++) {
+		parentSet = listOfParents[i];
+		
+		tempTruth = false;
+		
+		for(var x=0; x < parentSet; x++){
+			parent = parentSet[x];
+			
+			if(parent.length){
+				//select,option,buttons
+				if ($(parent).val() != choice)
+					tempTruth = true;
+			}
+		}
+		
+		if(tempTruth)
+			return true
+	}
+	return false;
 }
 
 $(function() {
