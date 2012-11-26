6595c4fd0595ce4057aca015060e69552296979e
Jay McEntire <jay.mcentire@gmail.com>
2012-11-26 16:49:31 -0700

    Fix for file handler to write files.

--- dataforms/utils/file.py
+++ dataforms/utils/file.py
@@ -10,13 +10,13 @@ class DataFormFile(File):
 	"""
 	Data Form File object that is used for FileField obecjts to represent a file.
 	"""
-	
+
 	def __init__(self, file, name=None):
 		super(DataFormFile, self).__init__(file, name)
 		self.path_name = self.name
 		self.name = self.name.split('/')[-1]
-		
-	
+
+
 	def _get_url(self):
 		return ''.join([settings.MEDIA_URL, self.path_name])
 	url = property(_get_url)
@@ -31,7 +31,7 @@ def handle_upload(files, field_key, folder=''):
 	# that this is a file that already exists, we don't need to upload anything.
 	if upload.__class__.__name__ == 'DataFormFile':
 		return upload.path_name
-	
+
 	# Make sure the upload path exists, and create it if not.
 	# Raises an OSError if the upload path does not exists
 	if not os.path.exists(upload_full_path):
@@ -45,11 +45,10 @@ def handle_upload(files, field_key, folder=''):
 	# Append an underscore to the given filename until the name is unique
 	while os.path.exists(os.path.join(upload_full_path, upload.name)):
 		upload.name = '_' + upload.name
-	dest = open(os.path.join(upload_full_path, upload.name), 'wb')
-	
+
 	# Write the uploaded file
-	for chunk in upload.chunks():
-		dest.write(chunk)
-	dest.close()
-	
+	with open(os.path.join(upload_full_path, upload.name), 'wb') as dest:
+		for chunk in upload.chunks():
+			dest.write(chunk)
+
 	return os.path.join(upload_dir, upload.name)
